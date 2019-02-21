df8a264851e534365e1dff8eea160a9e1221542e
Sandy-mr <sandy.mulato@gmail.com>
2019-02-20 20:22:05 -0600

    model

---
+++ src/models/Property.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+// Setup schema
+var propertySchema = mongoose.Schema({
+    name: {
+        type: String,
+    },
+    lastname: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export property model
+var Property = module.exports = mongoose.model('property', propertySchema);
+module.exports.get = function (callback, limit) {
+    Property.find(callback).limit(limit);
+}
